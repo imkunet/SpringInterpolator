6d26bae3cfd74d7d96db82bf24ea2b93ee5fff8a
kuuube <hexagonisalie@gmail.com>
2022-01-14 23:08:22 -0500

    Update to 0.6.0

--- SpringInterpolator/SpringInterpolator.cs
+++ SpringInterpolator/SpringInterpolator.cs
@@ -1,19 +1,19 @@
 ﻿using System.Numerics;
 using OpenTabletDriver.Plugin.Attributes;
-using OpenTabletDriver.Plugin.Tablet.Interpolator;
-using OpenTabletDriver.Plugin.Timers;
+using OpenTabletDriver.Plugin.Output;
+using OpenTabletDriver.Plugin.Tablet;
 
 namespace SpringInterpolator
 {
     [PluginName("SpringInterpolator")]
-    public class SpringInterpolator : Interpolator
+    public class SpringInterpolator : AsyncPositionedPipelineElement<IDeviceReport>
     {
         private readonly SpringSettings _settings;
         // future idea: make the z springy (pressure)
         private readonly Spring2D _spring;
-        private SyntheticTabletReport _report;
+        private uint pressure;
 
-        public SpringInterpolator(ITimer scheduler) : base(scheduler)
+        public SpringInterpolator()
         {
             // improvement/tweak: make the default target the current position of the cursor
             _settings = new SpringSettings(0.4f, 0.3f, 100f);
@@ -48,16 +48,29 @@ namespace SpringInterpolator
             set => _settings.Divide = value;
         }
 
-        public override SyntheticTabletReport Interpolate()
+        protected override void UpdateState()
         {
-            _report.Position = _spring.Update();
-            return _report;
+            if (State is ITabletReport _report)
+            {
+                _report.Position = _spring.Update();
+                _report.Pressure = this.pressure;
+                State = _report;
+            }
+
+            if (PenIsInRange())
+            {
+                OnEmit();
+            }
         }
 
-        public override void UpdateState(SyntheticTabletReport report)
+        protected override void ConsumeState()
         {
-            _spring.UpdateTarget(new Vector2(report.Position.X, report.Position.Y));
-            _report = report;
+            if (State is ITabletReport _report)
+            {
+                _spring.UpdateTarget(new Vector2(_report.Position.X, _report.Position.Y));
+                this.pressure = _report.Pressure;
+            }
         }
+        public override PipelinePosition Position => PipelinePosition.PreTransform;
     }
 }
